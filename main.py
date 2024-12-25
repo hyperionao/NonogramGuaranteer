15924b0358f08fde8b22cbebcf4ceb08b7630ddf
Aaron Ordonez <ordonezaaron67@gmail.com>
2024-12-25 03:12:53 -0500

    simplified code

--- main.py
+++ main.py
@@ -1,54 +1,51 @@
-length = input("Give the row/column length you are dealing with!\n")
-runs = input("Give the runs as shown above/aside the row/column! Format should be as a continuous string: 4 5\n")
+def calculate_blanks(length, runs_list):
+    return length - (sum(runs_list) + (len(runs_list) - 1))
 
-length = int(length)
-runs_list = runs.split()
-runs_list = [int(run) for run in runs_list]
+def process_input():
+    try:
+        length = int(input("Give the row/column length you are dealing with!\n"))
+        runs = input("Give the runs as shown above/aside the row/column! Format should be as a continuous string: 4 5\n")
+        runs_list = [int(run) for run in runs.split()]
+        return length, runs_list
+    except ValueError:
+        print("Invalid input. Please try again.")
+        exit()
 
-gap_array = ['X']
+length, runs_list = process_input()
+blanks = calculate_blanks(length, runs_list)
 
-#base case: runs with gaps already add up to length of row
-if sum(runs_list) + 1*(len(runs_list)-1) == length:
+if blanks < 0:
+    print("The row is overconstrained. Exiting.")
+    exit()
+elif blanks == 0:
     print("Already a perfect guarantee!\n")
-    
-#base case: runs will never have a guarantee slot
-if sum(runs_list) + 1*(len(runs_list)-1) <= length/2:
+elif sum(runs_list) + 1 * (len(runs_list) - 1) <= length / 2: #gaps+runs only reach half or is less than half, no guarantees possible! 
     print("No guarantees!")
 
-#calculating blanks 
-blanks = length - (sum(runs_list) + 1*(len(runs_list)-1))
-
-#all other cases
+#logic for gaps + 'runs'
 array_list = []
 for run in runs_list:
-    array_list.append(gap_array)
-    array_list.append([1]*run)
-array_list.pop(0)
-
-array_list.append(['_']*blanks)
+    array_list.append(['X'])
+    array_list.append([1] * run)
+array_list.pop(0)  # removes initial gap
+array_list.append(['_'] * blanks)
 
 for i, run in enumerate(array_list):
-    if i%2 == 0:
+    if i % 2 == 0: #strictly handles runs in the array_list
         if len(run) < blanks:
-            blank_ge_run = [0]*len(run)
-            array_list[i] = blank_ge_run
+            array_list[i] = [0] * len(run)
         else:
-            blank_run = [0]*blanks
-            run[:blanks] = blank_run
-        
-print(array_list)
+            run[:blanks] = [0] * blanks
 
 full_list = []
-for run in array_list:
-    full_list = full_list + run
-    
-    
 guarantee_count = 0
-for i, num in enumerate(full_list):
-    if num != 1:
-        full_list[i] = '?'
-    else: 
-        guarantee_count+=1
-        
-print(f'''{guarantee_count} guaranteed slots found: \n''')
+for run in array_list:
+    for num in run:
+        if num == 1:
+            full_list.append(1)
+            guarantee_count += 1
+        else:
+            full_list.append('?')
+
+print(f"{guarantee_count} guaranteed slots found: \n")
 print(full_list)
